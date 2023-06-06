72b9d67856756446dc8c70989c33a8d93b597712
M.Baumgärtner <klagges2011@googlemail.com>
2023-06-06 16:19:54 +0200

    Create db.sql
    Tabellen erstellt, FKs noch nicht Komplett

---
+++ sql/db.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE webshop;
+use webshop;
+CREATE TABLE webshop.User(
+    userID int PRIMARY KEY AUTO_INCREMENT,
+    VorName varchar(255),
+    NachName varchar(255),
+    Adresse varchar(255),
+    Telefon varchar(25),
+    EmailAdresse varchar(50),
+    LoginName varchar(50),
+    PasswortHash varchar(255),
+    istAdmin boolean
+);
+
+CREATE TABLE webshop.Kategorie(
+    kategorieID int PRIMARY KEY AUTO_INCREMENT,
+    bezeichnung varchar(255) NOT NULL,
+
+);
+
+CREATE TABLE webshop.Artikel(
+
+
+    artikelID int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    preis float(10),
+    bild BLOB,
+    beschreibung varchar(255),
+    verfuegbar boolean,
+    hersteller varchar(255),
+    kategorieA varchar(15),
+    kategorieB varchar(15),
+    kategorieC varchar(15)
+    )
+
+CREATE TABLE webshop.bestellungen(
+    bestellungID int PRIMARY KEY AUTO_INCREMENT,
+    datum date,
+    const_userID int,
+    artikelID int
+)
+
+CREATE TABLE webshop.BestellungenPos(
+    BID int,
+    AID int,
+    anzahl int
+)
+
+ALTER TABLE webshop.bestellungen
+ADD CONSTRAINT const_userID FOREIGN KEY (webshop.User) REFERENCES userID userID;
