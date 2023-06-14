3765029e70f30ef0f6ff39fd8bcf0691b7b42d2e
wild-fluffy-cloud <108592548+wild-fluffy-cloud@users.noreply.github.com>
2023-06-14 13:14:20 +0200

    14.06

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?PHP
 session_start();
 include("php/admin/util.inc.php");
@@ -175,4 +176,150 @@ $start_from = ($page - 1) * $res_per_page;
 </div>
 
 </body>
+=======
+<?PHP
+
+include("php/admin/util.inc.php");
+$page = '';
+
+// Pfüft die GET Variable, ob gesetzt und vom Typ integer
+if (isset($_GET['page']) && ctype_digit(strval($_GET['page']))) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+// Ergebnisse pro Seite
+$res_per_page = 4;
+$start_from = ($page - 1) * $res_per_page;
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="css/index.css">
+    <title>Sitzathlet</title>
+</head>
+<body>
+
+<div class="grid-container">
+
+    <!-- Header -->
+    <div class="item-1">
+        <h1>Sitzathlet Web-Shop</h1>
+        <nav class="navi">
+            <!--      <li>Hallo</li>-->
+
+            <li><a href="">Aktuelle Angebote </a></li>
+            <li><a href="/php/search.php">Suche</a></li>
+            <li><a href="/php/cart.php">Warenkorb</a></li>
+            <li><a href="/php/login.php">Login</a></li>
+            <li><a href="/php/admin/adminpanel.php">AdminPanel</a></li>
+        </nav>
+    </div>
+
+    <!-- Menu -->
+    <div class="sidebar">
+        <h2>Menu</h2>
+        <ol>Auswahl 1</ol>
+        <ol>Auswahl 2</ol>
+        <ol>Auswahl 3</ol>
+        <ol>Auswahl 4</ol>
+    </div>
+
+    <!-- Contet -->
+    <!-- php part mit while zum auslesen der Daten aus der SB -->
+    <?PHP
+    $conn = new_db_connect();
+
+    $sql = "SELECT * FROM Artikel limit ?,?";                                           // Prepared Statement
+    $stmt = $conn->prepare($sql);                                                       // Prepared Statement
+    $stmt->bind_param("dd", $start_from, $res_per_page);               // Prepared Statement
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+
+    if (!$result) {
+        die('Could not query:' . $conn->error);                                      // Wenn es keine ergbnis gibt wird Fehler ausgeggeben
+    }
+    $durchlauf = 0;
+    while ($row = $result->fetch_array()) {
+//            echo $durchlauf;
+        ?>
+
+
+        <div class="artikel" id="<?php echo getItemnr($durchlauf); ?>">
+            <table>
+                <tr> <!-- Zeile 1 -->
+                    <td rowspan="4">
+                        <?PHP echo ' <img height="300em" width="300em" src="data:image/jpeg;base64,' . base64_encode($row['bild']) . '"/>'; ?>
+                    </td>
+                    <td>
+                        Artikel:
+
+                    </td>
+                    <td>
+                        <?PHP echo($row['name']); ?>
+                    </td>
+                </tr>
+                <tr>    <!-- Zeile 2 -->
+                    <td rowspan="2">
+                        Artikelbeschreibung:
+                    </td>
+                    <td rowspan="2">
+                        <?PHP echo($row['beschreibung'] . "€"); ?>
+
+                    </td>
+
+                </tr>
+                <tr></tr>
+                <tr>    <!-- Zeile 3 -->
+                    <td>
+                        Preis:
+                    </td>
+                    <td>
+                        <?PHP echo($row['preis'] . "€"); ?>
+                        <a href="" ><img heigt="30em" width="30em" src="img/shopping-cart_full.png"</a>
+                    </td>
+                </tr>
+
+                <!--            <p>Artikel Beschreibung Preis</p>-->
+            </table>
+        </div>
+
+        <?php
+        $durchlauf++;  // Wird für die Function getTemnr(); benötigt um das entsprechende Grid ID zuzuweisen
+    }
+    ?>
+
+
+    <!-- Footer -->
+    <div class="item-7">
+        <?PHP
+        // SQL Abfrage für menge
+        $db = "select * from Artikel";
+        $result = $conn->query($db);
+        // Anzahl der Ergebnisse aus SQL Abfrage
+        $total_records = $result->num_rows;
+        // Ergebnisse gesamt durch Ergebnisse pro Seite teilen
+        $total_pages = ceil($total_records / $res_per_page);
+        $conn->close();
+        for ($i = 1; $i <= $total_pages; $i++) {
+            echo "<a href='?page=" . $i . "'>Seite " . $i . "</a> ";
+        }
+        // Letzt Seite
+        echo "<a href='?page=$total_pages'>" . '[Ende]' . "</a></p>";
+
+        echo '</div>';
+        ?>
+        <?php // $seite = $seite + 4; ?>
+
+    </div>
+
+</div>
+
+</body>
+>>>>>>> Stashed changes
 </html>
--- php/admin/func_upload.php
+++ php/admin/func_upload.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 // Datei in SQL Database hochladen
 include("util.inc.php");
@@ -42,4 +43,50 @@ if (!empty($_FILES['image'])) {
 header('location: neueartikel.php');  // nach dem Upload back to admin
 
 
+=======
+<?php
+// Datei in SQL Database hochladen
+include("util.inc.php");
+
+if (!empty($_FILES['image'])) {
+    $conn = new_db_connect();
+
+    if ($conn->connect_error) {
+        echo "Databayse connection Error";
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if (count($_FILES) > 0) {
+        if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+            // uebergabe der daten über die FROM der vor Seite
+            $imgData = file_get_contents($_FILES['image']['tmp_name']);
+            $imgType = $_FILES['image']['type'];
+            $name = $_POST['artikelName'];
+            $preis = $_POST['preis'];
+            $beschreibung = $_POST['beschreibung'];
+            $hersteller = $_POST['hersteller'];
+            $verfuegbar = $_POST['verfuegbar'];
+            $kategoriea = $_POST['kategoriea'];
+            // Build Prepared Statement
+            $sql = "INSERT INTO Artikel(bildType ,bild,name,preis, beschreibung, hersteller,verfuegbar, kategorieA) VALUES(?, ?,?,?,?,?,?,? )"; // @todo die restlichen einträge nachzihen.
+            $statement = $conn->prepare($sql);
+            $statement->bind_param('sssdssii', $imgType, $imgData, $name, $preis, $beschreibung, $hersteller, $verfuegbar, $kategoriea);
+            //i - integer
+            //d - double
+            //s - string
+            //b - BLOB
+            // execute PS
+            $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
+        }
+    }
+
+
+    $conn->close();
+
+}
+
+header('location: neueartikel.php');  // nach dem Upload back to admin
+
+
+>>>>>>> Stashed changes
 ?>
