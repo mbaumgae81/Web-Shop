6d7c7f0cb3f1d22d2ada8dccf407e3d17b16c4bf
wild-fluffy-cloud <108592548+wild-fluffy-cloud@users.noreply.github.com>
2023-06-15 12:34:17 +0200

    Add files via upload

---
+++ kategorie(1).sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 15. Jun 2023 um 12:33
+-- Server-Version: 10.4.28-MariaDB
+-- PHP-Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `webshop`
+--
+
+--
+-- Daten für Tabelle `kategorie`
+--
+
+INSERT INTO `kategorie` (`kategorieID`, `bezeichnung`) VALUES
+(1, 'Cardio'),
+(2, 'Kraft'),
+(3, 'Koordination');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
